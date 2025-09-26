c87101c4a6775fa0f413244cba27845fe8d586fb
brenda njoki <ngunjiribrenda5@gmail.com>
2025-09-26 23:06:03 +0300

    Creates Frontend Home.jsx

---
+++ PROJECT-4/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+        <div>
+            <h1>Welcome to the Digital Library App</h1>
+            <p>Explore a collection of books and view their details.</p>
+        </div>
+    );
+}
+
+export default Home;
