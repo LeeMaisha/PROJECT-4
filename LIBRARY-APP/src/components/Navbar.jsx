99a53726de9b09e6e038724920c1d61f3e864de1
brenda njoki <ngunjiribrenda5@gmail.com>
2025-09-27 19:46:07 +0300

    Adds navbar.jsx and updates data

---
+++ LIBRARY-APP/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-brand">
+        <Link to="/"> Digital Library App</Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/books">Browse Books</Link></li>
+        <li><Link to="/add-book">Add Book</Link></li>
+        <li><Link to="/genres">Genres</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
