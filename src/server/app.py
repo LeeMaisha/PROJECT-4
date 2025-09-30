1cbbda2922c0d4eb5014844f6e261437343e45de
Leonard Maisha <me@example.com>
2025-09-30 10:45:38 +0300

    adds changes

--- src/server/app.py
+++ src/server/app.py
@@ -10,7 +10,7 @@ import os
 from models import User, Book, Genre, Rating, BorrowRecord
 
 
-app = Flask(__name__, static_folder="../client/dist/assests", template_folder ="../client/dist")
+app = Flask(__name__, static_folder="../client/build/assests", template_folder ="../client/build")
 
 CORS(app)
 db_url = os.getenv('DATABASE_URL', 'sqlite:///library.db')
